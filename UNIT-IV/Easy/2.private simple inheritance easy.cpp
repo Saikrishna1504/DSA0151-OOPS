56b45c9b60d83e9fbc484777961a3166a8e43799
Sai krishna <107762536+Saikrishna1504@users.noreply.github.com>
2024-07-01 20:23:06 +0530

    add comments

--- UNIT-IV/Easy/2.private simple inheritance easy.cpp
+++ UNIT-IV/Easy/2.private simple inheritance easy.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
-
 using namespace std;
 
+// Base class A with a protected member variable
 class A {
 protected:
     int x;
+
 public:
+    // Constructor that initializes x with the given value
     A(int value) : x(value) {}
 };
 
+// Derived class B that inherits from class A
 class B : private A {
 public:
+    // Constructor that calls the base class constructor
     B(int value) : A(value) {}
 
+    // Function to display the value of x
     void showValue() {
         cout << "Value of x: " << x << endl;
     }
 };
 
 int main() {
-    B obj(10);
-    obj.showValue();
+    B obj(10); // Create an object of class B with value 10
+    obj.showValue(); // Call the function to display the value of x
 
-    return 0;
+    return 0; // Return 0 to indicate successful execution
 }
