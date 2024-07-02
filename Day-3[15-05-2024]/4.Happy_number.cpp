be0a1386f7e1242f2b621ceb8c85dd0fe392bac0
Sai krishna <107762536+Saikrishna1504@users.noreply.github.com>
2024-07-02 06:29:54 +0530

    Update 4.Happy_number.cpp

--- Day-3[15-05-2024]/4.Happy_number.cpp
+++ Day-3[15-05-2024]/4.Happy_number.cpp
@@ -32,3 +32,31 @@ int main() {
     }
     return 0;
 }
+
+
+#include <set>
+using namespace std;
+
+// Function to calculate the sum of the squares of the digits of a number
+int numSquareSum(int n) {
+    int num = 0; // Variable to store the sum of the squares of the digits
+    while (n != 0) { // Loop through each digit of the number
+        int digit = n % 10; // Get the last digit of the number
+        num += digit * digit; // Add the square of the digit to the sum
+        n /= 10; // Remove the last digit from the number
+    }
+    return num; // Return the sum of the squares of the digits
+}
+
+// Function to check if a number is a Happy Number
+int isHappyNumber(int n) {
+    set<int> st; // Set to store the numbers we have seen to detect cycles
+    while (1) { // Infinite loop to keep checking the number
+        n = numSquareSum(n); // Get the sum of the squares of the digits of n
+        if (n == 1) // If n becomes 1, it is a Happy Number
+            return true; // Return true indicating it is a Happy Number
+        if (st.find(n) != st.end()) // If n is already in the set, a cycle is detected
+            return false; // Return false indicating it is not a Happy Number
+        st.insert(n); // Insert the current value of n into the set
+    }
+}
